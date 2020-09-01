1eb16ff004bbd3a30d71acce8993927b77e94b78
Minorin <tekotekowalknyan@icloud.com>
2020-09-01 21:14:28 +0900

    add Color.vue

---
+++ src/components/Color.vue
@@ -0,0 +1,33 @@
+<template>
+  <div class="box" id="color">
+    <h5 class="tool_title"><font-awesome-icon icon="palette" /> Color</h5>
+    <sketch-picker :value="colors"></sketch-picker>
+  </div>
+</template>
+
+<script>
+import { Sketch } from 'vue-color'
+export default({
+  el: '#app',
+  components: {
+   'sketch-picker': Sketch,
+  },
+  data() {
+  return {
+   colors: {
+     hex: '#1f32a6',
+     a: 1
+   }
+    }
+ }
+});
+</script>
+
+<style scoped>
+.vc-sketch {
+  background: transparent !important;
+  border-radius: 0% !important;
+  box-shadow: none !important;
+  padding-top: 0 !important;
+}
+</style>
