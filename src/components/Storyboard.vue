734e3274c3b15fd593cb2d37aa066c82902e2cd8
TO <takahiro.ohori@gmail.com>
2020-09-08 21:23:55 +0900

    update: 読み込んだファイルをstoryboadに反映

--- src/components/Storyboard.vue
+++ src/components/Storyboard.vue
@@ -1,11 +1,10 @@
 <template>
-  <div class="box" id="storyboard">
-    <div>{{htmlText}}</div>
-  </div>
+  <div class="box" id="storyboard" v-html="htmlText"></div>
 </template>
 
 <script>
-const { ipcRenderer } = require("electron");
+const { ipcRenderer, remote } = require("electron");
+const fs = remote.require("fs");
 
 export default {
   data: function () {
@@ -15,11 +14,16 @@ export default {
   },
   computed: {
     htmlText: function () {
+      if (this.filePath === "") {
+        return "";
+      }
+
       // ファイルデータを読み込む
+      const data = fs.readFileSync(this.filePath, "utf-8");
 
-      // 関係ないところをカットする
-
-      return this.filePath + "html";
+      // bodyのみを取り出す
+      const bodyText = this.extractText(data, "<body>", "</body>");
+      return bodyText;
     },
   },
   mounted: function () {
@@ -30,8 +34,73 @@ export default {
       this.filePath = filePath;
     });
   },
+  methods: {
+    extractText: function (text, start, end) {
+      // startの文字の位置
+      const startPos = text.indexOf(start) + start.length;
+
+      // endの文字の位置
+      const endPos = text.indexOf(end);
+
+      // 間の文字列を切り取る
+      const result = text.slice(startPos, endPos);
+
+      return result;
+    },
+  },
 };
 </script>
 
 <style scoped>
+.box >>> .storyboard {
+  display: grid;
+}
+.box >>> .coma {
+  display: grid;
+  grid-template-columns: 60px 192px auto auto 100px;
+  min-height: 108px;
+}
+.box >>> .head {
+  background-color: #fff;
+  position: -webkit-sticky;
+  position: sticky;
+  top: 0;
+  z-index: 1000;
+  min-height: 28px !important;
+  border: 1px solid #00000038;
+}
+.box >>> .cut,
+.box >>> .picture,
+.box >>> .action,
+.box >>> .dialogue,
+.box >>> .time {
+  border: 1px solid #00000038;
+}
+.box >>> .head .cut,
+.box >>> .head .picture,
+.box >>> .head .action,
+.box >>> .head .dialogue,
+.box >>> .head .time {
+  border: none;
+}
+.box >>> .picture {
+  line-height: 0;
+  background-color: #fff;
+}
+.box >>> .action,
+.box >>> .dialogue {
+  min-width: 140px;
+}
+.box >>> h4 {
+  margin: 2px 4px;
+  line-height: 24px;
+}
+.box >>> img {
+  position: relative;
+  z-index: 2;
+  zoom: 0.1;
+}
+.box {
+  color: black;
+}
 </style>
